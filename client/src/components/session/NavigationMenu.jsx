c4b0d6328d273245fad5699dd18e53e721997779
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2021-04-16 21:19:37 +0200

    feat: Working implementation for navigation panel

--- client/src/components/session/NavigationMenu.jsx
+++ client/src/components/session/NavigationMenu.jsx
@@ -3,17 +3,46 @@ import '../../styles/NavigationMenu.css';
 import React from 'react';
 import NavigationMenuItem from './NavigationMenuItem.jsx';
 
+import prod_control from '../../assets/production_control.png';
+import tech_tree from '../../assets/tech_tree.png';
+import fleets_movements from '../../assets/fleet_movements.png';
+
 function NavigationMenu (props) {
   return (
     <div className="navigation_menu_layout">
-      <NavigationMenuItem name={"General view"} />
-      <NavigationMenuItem name={"Resources"} />
-      <NavigationMenuItem name={"Facilities"} />
-      <NavigationMenuItem name={"Research lab"} />
-      <NavigationMenuItem name={"Shipyard"} />
-      <NavigationMenuItem name={"Defenses"} />
-      <NavigationMenuItem name={"Fleets"} />
-      <NavigationMenuItem name={"Galaxy"} />
+      <NavigationMenuItem name={"General view"}
+                          item_link={"../overview/overview.html"}
+                          />
+      <NavigationMenuItem name={"Resources"}
+                          item_link={"../resources/resources.html"}
+                          data_link={"../resources/production_settings.html"}
+                          icon={prod_control}
+                          icon_alt={"Prod control"}
+                          />
+      <NavigationMenuItem name={"Facilities"}
+                          item_link={"../facilities/facilities.html"}
+                          />
+      <NavigationMenuItem name={"Research lab"}
+                          item_link={"../research_lab/research_lab.html"}
+                          data_link={"../research_lab/technologies_list.html"}
+                          icon={tech_tree}
+                          icon_alt={"Technology tree"}
+                          />
+      <NavigationMenuItem name={"Shipyard"}
+                          item_link={"../shipyard/shipyard.html"}
+                          />
+      <NavigationMenuItem name={"Defenses"}
+                          item_link={"../defenses/defenses.html"}
+                          />
+      <NavigationMenuItem name={"Fleets"}
+                          item_link={"../fleets/fleets_step_1.html"}
+                          data_link={"../fleets/fleets_movements.html"}
+                          icon={fleets_movements}
+                          icon_alt={"Fleets"}
+                          />
+      <NavigationMenuItem name={"Galaxy"}
+                          item_link={"../galaxy/galaxy.html"}
+                          />
     </div>
   );
 }
--- client/src/components/session/NavigationMenuItem.jsx
+++ client/src/components/session/NavigationMenuItem.jsx
@@ -5,12 +5,21 @@ import React from 'react';
 function NavigationMenuItem (props) {
   return (
     <div className="navigation_menu_item_layout">
-      <div class="navigation_menu_item_wrapper">
-        <a class="navigation_menu_item" href={props.item_link}>{props.name}</a>
+      <div className="navigation_menu_item_wrapper">
+        <a className="navigation_menu_item" href={props.item_link}>{props.name}</a>
+      </div>
+      {
+        props.data_link &&
+        <a className="navigation_menu_item_data" href={props.data_link}>
+          <img className="navigation_menu_item_icon" src={props.icon} alt={props.icon_alt} />
+        </a>
+      }
+      {
+        !props.data_link &&
+        <div className="navigation_menu_item_data">
+          <div className="navigation_menu_item_icon"/>
         </div>
-      <a class="navigation_menu_item_data" href={props.data_link}>
-        <img class="navigation_menu_item_icon" src={props.icon} alt={props.icon_alt} />
-      </a>
+      }
     </div>
   );
 }
