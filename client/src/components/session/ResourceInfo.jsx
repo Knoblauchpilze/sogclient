2d5ce016f66d8eafc3825595dcca1833935d8958
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2021-05-14 11:18:01 +0200

    feat: Refined the display of resources based on the storage capacity and production

--- client/src/components/session/ResourceInfo.jsx
+++ client/src/components/session/ResourceInfo.jsx
@@ -2,11 +2,32 @@
 import '../../styles/session/ResourceInfo.css';
 import React from 'react';
 
+function formatAmount(amount) {
+  let out = "";
+
+  if (amount > 0) {
+    out += "+";
+  }
+  out += Math.floor(amount);
+
+  return out;
+}
+
 function ResourceInfo (props) {
+  let classAmount = "resource_info_amount"
+  if (props.data.amount > props.data.storage && props.data.storable) {
+    classAmount += " resource_info_warning";
+  }
+  let classProduction = "resource_info_production";
+  if (props.data.production < 0) {
+    classProduction += " resource_info_warning";
+  }
+
   return (
     <div className="resource_info_layout">
-      <img src={props.icon} alt={props.alt_text} title={props.title} />
-      <p>{props.amount}</p>
+      <img src={props.data.icon} alt={props.data.title} title={props.data.title} />
+      <span className={classAmount}>{props.data.amount}</span>
+      <span className={classProduction}>{"(" + formatAmount(props.data.production) + ")"}</span>
     </div>
   );
 }
--- client/src/components/session/ResourcesDisplay.jsx
+++ client/src/components/session/ResourcesDisplay.jsx
@@ -3,84 +3,36 @@ import '../../styles/session/ResourcesDisplay.css';
 import React from 'react';
 import ResourceInfo from './ResourceInfo.jsx';
 
-import metal from '../../assets/metal_mini.jpeg';
-import crystal from '../../assets/crystal_mini.jpeg';
-import deuterium from '../../assets/deuterium_mini.jpeg';
-import antimatter from '../../assets/antimatter_mini.jpeg';
-import energy from '../../assets/energy_mini.jpeg';
+import { resources_list } from '../../datas/resources.js';
+
+function generateResourceDesc(res, fromPlanet, data) {
+  // Find the resource data.
+  const rDesc = data.find(r => r.name === res.name);
+  const rData = fromPlanet.find(r => r.resource === rDesc.id);
+
+  return {
+    title: res.name,
+    icon: res.mini,
+
+    amount: Math.floor(rData.amount),
+    production: rData.production,
+    storage: rData.storage,
+    storable: rDesc.storable,
+  };
+}
 
 function ResourcesDisplay (props) {
-  let ma = 0;
-  let ca = 0;
-  let da = 0;
-  let aa = 0;
-  let ea = 0;
-
   if (props.planet && props.resources) {
-    const mid = props.resources.find(r => r.name === "metal");
-    if (mid) {
-      const mAmount = props.planet.resources.find(r => r.resource === mid.id);
-      if (mAmount) {
-        ma = Math.floor(mAmount.amount);
-      }
-    }
-    const cid = props.resources.find(r => r.name === "crystal");
-    if (cid) {
-      const cAmount = props.planet.resources.find(r => r.resource === cid.id);
-      if (cAmount) {
-        ca = Math.floor(cAmount.amount);
-      }
-    }
-    const did = props.resources.find(r => r.name === "deuterium");
-    if (did) {
-      const dAmount = props.planet.resources.find(r => r.resource === did.id);
-      if (dAmount) {
-        da = Math.floor(dAmount.amount);
-      }
-    }
-    const aid = props.resources.find(r => r.name === "antimatter");
-    if (aid) {
-      const aAmount = props.planet.resources.find(r => r.resource === aid.id);
-      if (aAmount) {
-        aa = Math.floor(aAmount.amount);
-      }
-    }
-    const eid = props.resources.find(r => r.name === "energy");
-    if (eid) {
-      const eAmount = props.planet.resources.find(r => r.resource === eid.id);
-      if (eAmount) {
-        ea = Math.floor(eAmount.amount);
-      }
-    }
   }
 
   return (
     <div className="resources_display_layout">
-      <ResourceInfo icon={metal}
-                    alt_text={"Metal"}
-                    title={"Metal"}
-                    amount={ma}
-                    />
-      <ResourceInfo icon={crystal}
-                    alt_text={"Crystal"}
-                    title={"Crystal"}
-                    amount={ca}
-                    />
-      <ResourceInfo icon={deuterium}
-                    alt_text={"Deuterium"}
-                    title={"Deuterium"}
-                    amount={da}
-                    />
-      <ResourceInfo icon={antimatter}
-                    alt_text={"Antimatter"}
-                    title={"Antimatter"}
-                    amount={aa}
-                    />
-      <ResourceInfo icon={energy}
-                    alt_text={"Energy"}
-                    title={"Energy"}
-                    amount={ea}
-                    />
+      {
+        props.planet && props.resources &&
+        resources_list.map(r =>
+          <ResourceInfo key={r.name} data={generateResourceDesc(r, props.planet.resources, props.resources)} />
+        )
+      }
     </div>
   );
 }
