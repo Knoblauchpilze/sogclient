b1e0cbd480e965a6d4f0895d8ae2aa289f545279
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2021-05-30 11:17:42 +0200

    feat: Interpreted activity for galaxy view

--- client/src/components/session/GalaxyPlanet.jsx
+++ client/src/components/session/GalaxyPlanet.jsx
@@ -7,7 +7,61 @@ import planet from '../../assets/planet1.png';
 import moon from '../../assets/moon.png';
 import wreckfield from '../../assets/wreckfield.png';
 
+// Defines the number of milliseconds below which the
+// planet is considered as 'active'. The amount is
+// computed from converting 15 minutes to milliseconds.
+const PLANET_ACTIVE_INTERVAL = 15 * 60 * 1000;
+
+// Defines the number of milliseconds above which the
+// planet is considered as 'inactive'. The amount is
+// computed from converting 60 minutes to milliseconds.
+const PLANET_INACTIVE_INTERVAL = 60 * 60 * 1000;
+
+function generateActivityString(planet) {
+  // Parse the last activity.
+  const activity = Date.parse(planet.last_activity);
+  const now = Date.now();
+  const elapsed = now - activity;
+
+  // We display an asterisk in case the activity is more
+  // recent than 15 minutes.
+  if (elapsed < PLANET_ACTIVE_INTERVAL) {
+    return {
+      text: "*",
+      class: "galaxy_planet_activity",
+    };
+  }
+  if (elapsed > PLANET_INACTIVE_INTERVAL) {
+    return {
+      text: "",
+      class: "",
+    };
+  }
+
+  // Compute the number of minutes elapsed since the last
+  // activity.
+  const minutes = Math.floor(elapsed / (1000 * 60));
+
+  return {
+    text: "" + minutes,
+    class: "galaxy_planet_distant_activity",
+  };
+}
+
 function GalaxyPlanet (props) {
+  let activity = {
+    text: "",
+    class: "",
+  };
+  let classes = "galaxy_planet_base galaxy_planet_5";
+
+  if (props.planet.name !== "") {
+    activity = generateActivityString(props.planet);
+    if (activity.class !== "") {
+      classes += (" " + activity.class);
+    }
+  }
+
   return (
     <div className="galaxy_planet_layout">
       <span className="galaxy_planet_base galaxy_planet_1 galaxy_planet_label">{props.planet.coordinate.position + 1}</span>
@@ -16,9 +70,15 @@ function GalaxyPlanet (props) {
            alt={"planet_" + props.planet.coordinate.position}
            />
       <span className="galaxy_planet_base galaxy_planet_3 galaxy_planet_label">{props.planet.name}</span>
-      <img className="galaxy_planet_base galaxy_planet_4" src={moon} alt="moon"/>
-      <span className="galaxy_planet_base galaxy_planet_5 galaxy_planet_activity">*</span>
-      <img className="galaxy_planet_base galaxy_planet_6" src={wreckfield} alt="debris_gone"/>
+      <img className="galaxy_planet_base galaxy_planet_4"
+           src={moon}
+           alt="moon"
+           />
+      <span className={classes}>{activity.text}</span>
+      <img className="galaxy_planet_base galaxy_planet_6"
+           src={wreckfield}
+           alt="debris"
+           />
       <span className="galaxy_planet_base galaxy_planet_7 galaxy_planet_label">{props.planet.player_name}</span>
       <span className="galaxy_planet_base galaxy_planet_8 galaxy_planet_label">{props.guild}</span>
       <button className="galaxy_planet_base galaxy_planet_9 galaxy_planet_action galaxy_planet_spy"></button>
